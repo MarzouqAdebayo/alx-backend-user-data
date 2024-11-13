d2f10ace94e0922a03ca792e6e25753abee4e7f3
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 21:47:16 +0100

    :zap: added try/except block to login route handler

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -24,7 +24,10 @@ def login() -> Tuple[str, int]:
         return jsonify({"error": "password missing"}), 400
     from models.user import User
 
-    users = User.search({"email": user_email})
+    try:
+        users = User.search({"email": user_email})
+    except Exception:
+        return jsonify({"error", "no user found for this email"}), 404
     if len(users) == 0:
         return jsonify({"error", "no user found for this email"}), 404
     if not users[0].is_valid_password(user_pwd):
