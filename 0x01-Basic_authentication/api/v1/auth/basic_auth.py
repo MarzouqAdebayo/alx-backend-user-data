406a1bbd0d32cc4833587e03656e4f82d2fcc735
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 06:55:26 +0100

    :zap: Completed task 7 extracting base64 part of auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,4 +6,15 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Represents Basic Auth"""
 
-    pass
+    def extract_base64_authorization_header(
+        self,
+        authorization_header: str,
+    ) -> str:
+        """Extract base64 auth header"""
+        if (
+            authorization_header is None
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith("Basic ")
+        ):
+            return None
+        return authorization_header.split(" ")[1]
