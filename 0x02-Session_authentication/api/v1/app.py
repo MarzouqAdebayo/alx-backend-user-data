0dbe92715b620743fa8819bbdc17be6a2a352158
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 18:27:10 +0100

    :bug: Bug fixes in task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,7 +47,11 @@ def authenticate_user():
     """Authenticates a user before processing a request"""
     if auth is None:
         return None
-    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+    ]
     if not auth.require_auth(request.path, excluded_paths):
         return None
     user = auth.current_user(request)
