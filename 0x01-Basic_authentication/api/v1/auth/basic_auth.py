435e8d54cbbbac4f2570467a12d4b7831489547f
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 07:03:17 +0100

    :zap: Completed task 8, Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Module 'basic_auth.py' """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import dec
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,17 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str,
+    ) -> str:
+        if base64_authorization_header is None or not isinstance(
+            base64_authorization_header, str
+        ):
+            return None
+        try:
+            b64decode(base64_authorization_header)
+        except Exception:
+            return None
+        return base64_authorization_header.decode("utf-8")
