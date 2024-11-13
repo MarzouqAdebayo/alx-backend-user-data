515fb2075622e12280efed22c7cce080db809825
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 05:44:20 +0100

    :zap: Completed task 3 and 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,7 +8,11 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require_auth method"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if path in excluded_paths or f"{path}/" in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header method"""
