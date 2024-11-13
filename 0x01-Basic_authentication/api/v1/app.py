5c3146fde5fc23a79a6c6f9a683608700e4920e8
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 06:01:28 +0100

    :zap: fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -47,12 +47,8 @@ def authenticate_user():
     """Authenticates a user before processing a request"""
     if auth is None:
         return None
-    excluded_paths = [
-        "/api/v1/status/",
-        "/api/v1/unauthorized/",
-        "/api/v1/forbidden/"
-    ]
-    if auth.require_auth(request.path, excluded_paths):
+    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    if not auth.require_auth(request.path, excluded_paths):
         return None
     if auth.authorization_header(request) is None:
         abort(401)
