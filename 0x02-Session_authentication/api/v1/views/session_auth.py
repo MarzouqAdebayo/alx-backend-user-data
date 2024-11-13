de504d24ab9d94e9bbc5f495a40c8f8eda419c25
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 20:32:11 +0100

    :sparkles: Completed task 7 take 1

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module 'session_auth.py' """
+import os
+from typing import Tuple
+from flask import request, jsonify
+from api.v1.views import app_views
+from models import User
+
+
+@app_views.route(
+    "/auth_session/login",
+    methods=["POST"],
+    strict_slashes=False,
+)
+def login() -> Tuple[str, int]:
+    """POST /api/v1/auth_session/login
+    Return:
+        - JSON representation of a User object
+    """
+    user_email = request.form.get("email")
+    if not user_email or not len(user_email.strip()):
+        return jsonify({"error": "email missing"}), 400
+    user_pwd = request.form.get("password")
+    if not user_pwd or not len(user_pwd.strip()):
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({"email": user_email})
+    if len(users) == 0:
+        return jsonify({"error", "no user found for this email"}), 404
+    if not users[0].is_valid_password(user_pwd):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+
+    session_id = auth.create_session(getattr(users[0], "id"))
+    response = jsonify(users[0].to_json())
+    session_name = os.getenv("SESSION_NAME")
+    response.set_cookie(session_name, session_id)
+    return response
