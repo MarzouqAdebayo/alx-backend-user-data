a958ab57d87152794a74e1b15a88d738ac76f1be
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 19:17:07 +0100

    :bug: Returned session id

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -35,4 +35,4 @@ class Auth:
         if request is None:
             return None
         cookie_name = os.getenv("SESSION_NAME")
-        request.cookies.get(cookie_name)
+        return request.cookies.get(cookie_name)
