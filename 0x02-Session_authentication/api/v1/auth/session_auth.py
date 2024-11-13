acfa1c4057df5157659c7489a0212b3f4b797cd7
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-13 18:50:25 +0100

    :sparkles: Completed task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 """Module 'session_auth.py' """
+import uuid
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Represents Session Auth"""
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid()
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
