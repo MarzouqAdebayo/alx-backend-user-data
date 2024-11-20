091a72fc329574fa1297bf0484f911e16bd8352a
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-20 21:38:25 +0100

    :sparkles: no change, just pushing again

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """user module contains user class"""
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -12,7 +12,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String(50), nullable=False)
+    email = Column(String(255), nullable=False)
     hashed_password = Column(String(255), nullable=False)
     session_id = Column(String(255), nullable=True)
     reset_token = Column(String(255), nullable=True)
