afd5c1a73717521398b93b3e44f10fb3abbca836
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-22 21:09:56 +0100

    :sparkles: Completed tasks last to 8

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module 'app.py' Contains a basic Flask app with auth"""
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /"""
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """POST /users"""
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Module 'auth.py' """
+import bcrypt
+from db import DB
+from sqlalchemy.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes password and returns bytes"""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """Registers a new user"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Logs in a user using their email and password"""
+        try:
+            self._db.find_user_by(email=email)
+        except Exception:
+            return False
+        return bcrypt.checkpw(
+            password.encode("utf-8"),
+            _hash_password(password),
+        )
