748b26136175148fc6f1189fd8cebcb10f106bdb
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-22 21:47:11 +0100

    :sparkles: Completed task 11 take 1

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module 'app.py' Contains a basic Flask app with auth"""
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -24,6 +24,23 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """POST /sessions"""
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        valid_login = AUTH.valid_login(email, password)
+        if not valid_login:
+            abort(401)
+        session_id = AUTH.create_session(email)
+    except ValueError:
+        return jsonify()
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
