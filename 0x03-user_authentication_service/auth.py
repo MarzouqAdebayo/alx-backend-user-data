b43886051585421ed8fe101ff148acfef7909ffe
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-23 08:55:02 +0100

    Testing if guard in get_user_from_session_id works?

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,8 +55,6 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Get user by their session_id"""
-        if session_id is None:
-            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
         except Exception:
